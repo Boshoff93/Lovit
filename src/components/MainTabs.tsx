f77624b5f758852aed42f20c0c7f2546af8a626a
Boshoff93 <wiehan.boshoff@gmail.com>
2025-04-26 01:07:32 -0700

    S

--- src/components/MainTabs.tsx
+++ src/components/MainTabs.tsx
@@ -432,7 +432,7 @@ const MainTabs: React.FC = () => {
       console.log(`Checking WebSockets for ${generatingImages.length} existing generating images`);
       
       generatingImages.forEach(img => {
-        console.log(`Generating image: ${img.id}`);
+        console.log(`Generating image: ${JSON.stringify(img)}`);
         // Only connect if we haven't already connected to this image AND the image has a valid ID
         if (img.id && typeof img.id === 'string' && !connectedImageIdsRef.current.has(img.id)) {
           console.log(`Connecting to WebSocket for image ${img.id}`);
