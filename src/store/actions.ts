3d4b631461b9340d2980ea455d73342045632f65
Wiehan <wiehan@Wiehans-MacBook-Pro.local>
2025-07-08 22:33:07 -0700

    ds

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,10 +1,9 @@
-import { AppDispatch } from './store';
 import { logout as authLogout } from './authSlice';
 import { clearModels } from './modelsSlice';
 import { clearGallery } from './gallerySlice';
 
 // Shared logout action that clears all relevant state
-export const logoutAllState = () => (dispatch: AppDispatch) => {
+export const logoutAllState = () => (dispatch: any) => {
   dispatch(authLogout());
   dispatch(clearModels());
   dispatch(clearGallery());
